fff767b4ac1a3eec1cdcea924da786463c1f2e6e
LeoERP <leona.rodriguez1985@gmail.com>
2023-09-12 15:09:07 +0200

    nos mudamos

---
+++ hackanew/BACKEND/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const hackanews = require('hackanews');
+
+app.use(hackanews);
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('Pasa una petición.');
+    req.cohete = '🚀';
+    next();
+});
+app.get('/', (req, res) => {
+    res.status(200).send(`${req.cohete} Bienvenido a Hack a News ${req.cohete}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+
+})
