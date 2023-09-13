f6b3310bbd3cf945d5098bd3bb158ff34c71578a
Nacho <136459268+Nacho131192@users.noreply.github.com>
2023-09-13 16:35:02 +0200

    actualizacion adduser

--- hackanew/BACKEND/controller/users/addUser.js
+++ hackanew/BACKEND/controller/users/addUser.js
@@ -1,5 +1,8 @@
+const { log } = require('console');
 const sendQuery = require('../../db/connecToDB');
 const newUserSchema = require('../../schemas/newUserSchema');
+const bcrypt = require('bcrypt');
+
 
 function addUser(req, res, next) {  // funcion que manda los datos a la base de datos
 
@@ -8,7 +11,37 @@ function addUser(req, res, next) {  // funcion que manda los datos a la base de 
         return next(createError(400, error.message));
     }
 
-    const { name, email, password } = req.body;
+    const { user_name, user_email, user_password } = req.body;
+
+
+
+    try {   //* COMPRUEBA MAIL Y USER
+        const [user_name] = await sendQuery('SELECT name FROM users WHERE name = ?', [user_name]);
+        if (user_name) {
+            return next(createError(409, 'El nombre de usuario ya existe'));
+        }
+        const [user_email] = await sendQuery('SELECT email FROM users WHERE email = ?', [user_email]);
+        if (user_email) {
+            return next(createError(409, 'El email ya existe'));
+        }
+        const hashedPassword = await bcrypt.hash(user_password, 10);
+        const result = await sendQuery('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [user_name, user_email, hashedPassword]);
+
+        console.log(result);
+
+        res.send({
+            status: 'success',
+            userid: result.insertId,
+            user_name,
+            user_email,
+
+        });
+    }
+
+
+    catch (error) {
+        return next(createError(500, error.message));  //! PENDIENTE DE CREAR ERROR PERSONALIZADO
+    }
 
 }
 
