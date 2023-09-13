4e12440816351ecb8d83316b07e4775eb9fe2c03
Nacho <136459268+Nacho131192@users.noreply.github.com>
2023-09-13 18:13:24 +0200

    autenticator1

--- hackanew/BACKEND/middlewares/authUser.js
+++ hackanew/BACKEND/middlewares/authUser.js
@@ -1,7 +1,23 @@
 const createError = require('../helpers/createError');
+const jwt = require('jsonwebtoken');
 
-function authUser(req, res, next) {
 
+async function authUser(req, res, next) {
+    const { 'x-access-token': token } = req.headers;
+    if (!token) {
+        return next(createError(401, 'No autenticado'));
+    }
 
+    let infoUser;
+    try {
+        infoUser = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        return next(createError(401, 'Token incorrecto'));
+    }
+
+    req.user = infoUser;
     next();
 }
+
+
+module.exports = authUser;
