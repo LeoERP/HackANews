5fb14063f82c3eac68d8a39665f61c24ea2c778b
Nacho <136459268+Nacho131192@users.noreply.github.com>
2023-09-14 18:26:03 +0200

    actualizacion entrada de comentario

--- hackanew/BACKEND/routes/entriesRouter.js
+++ hackanew/BACKEND/routes/entriesRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const authUser = require('../middlewares/authUser');
-
+const addEntry = require('../controllers/entries/addEntry');
 
 
 
 router.get('/', __________);    //!COMPLETAR PARTE DE MOSTRAR LAS ENTRADAS DE FORO
-router.post('/', authUser, ___________);  //! PARA GCAER LAS ENTRADAS DE FORO
+router.post('/', authUser, addEntry);
 router.delete('/:entryId', authUser, ___________);  //! PARA ELIMINAR LAS ENTRADAS DE FORO
 
 
