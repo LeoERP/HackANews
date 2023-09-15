e364662e8c9315a1781b1fb339b889414b812a03
LeoERP <leona.rodriguez1985@gmail.com>
2023-09-15 11:21:14 +0200

    picando usersRouter

--- hackanew/BACKEND/routes/usersRouter.js
+++ hackanew/BACKEND/routes/usersRouter.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+const { loginUser } = require('../controllers/users');
+
+
 usersRouter.post('/login', loginUser);
+
+module.exports = usersRouter;
+
